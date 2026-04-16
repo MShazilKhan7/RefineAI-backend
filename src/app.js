c8132767f6c58e8d80dff3c88658d177a24cd344
Muhammad Shazil <shazilk14@gmail.com>
2026-04-16 22:52:08 +0500

    solved cors origin

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(
       "http://localhost:8080",
       "http://localhost:5001",
       "http://localhost:5173",
+      "https://refineaiapp.vercel.app"
     ],
     credentials: true,
   }),
