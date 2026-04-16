9080cb3ee91da785d3655f49fd20d653d93bc7fa
Muhammad Shazil <shazilk14@gmail.com>
2026-04-16 21:38:52 +0500

    added ai deployed url

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const AI_BASE_URL = "http://127.0.0.1:8000/ai";
+const AI_BASE_URL = process.env.AI_BASE_URL
 
 // Suggestions API
 export const generateSuggestionsAI = async (
